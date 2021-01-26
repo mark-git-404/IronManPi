b20c3e1bd3748a236d9f5ae284ddedabd66d8bb3
Marcos Gabriel <marcos.gabriel@ossirian.com>
2021-01-26 13:07:01 -0300

    main

--- IronMan.c
+++ IronMan.c
@@ -3,12 +3,14 @@
 #define GPIO2 3;
 int main (void)
 {
-  wiringPiSetup () ;
-  pinMode (GPIO2, OUTPUT) ;
-  for (;;)
+  wiringPiSetup();
+  pinMode(GPIO2, OUTPUT);
+  for(i = 1; i < 11; ++i)
   {
-    digitalWrite (GPIO2, HIGH) ; delay (500) ;
-    digitalWrite (GPIO2,  LOW) ; delay (500) ;
+    digitalWrite(GPIO2, HIGH); 
+    delay(500);
+    digitalWrite(GPIO2, LOW);
+    delay(500);
   }
   return 0 ;
 }
